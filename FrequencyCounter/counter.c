0df4e11c85717752f33058cf1b0bfaea834a2358
Nikhil-Shenoy <nikhil_shenoy10@yahoo.com>
2015-02-03 16:55:57 -0500

    started to process command line options

--- FrequencyCounter/counter.c
+++ FrequencyCounter/counter.c
@@ -1,58 +1,32 @@
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
-#include "include/func.h"
-
-/*
-char *lowerCase(char *arg) {
-
-	int i;
-	i = 0;
-
-	while(i != strlen(arg)) {
-		arg[i] = tolower(arg[i]);
-		i++;
-	}
-
-	return arg;
-}
-
-
-// This function counts the frequency of a given string in the given file.
-int freqCount(FILE *source, char *string) {
-
-	int counter;
-	counter = 0;
-	char buffer[strlen(string) + 1]; // space to hold candidates from the file
-	
-	char c;
-	while( (c = fgetc(source)) != EOF) { // Get character from file
-		if(c == string[0]) { // If starting characters match, begin the processing
-			fseek(source,-1,SEEK_CUR);
-			fgets(buffer,strlen(string)+1,source); // get the candidate string from file
-			if(strcmp(lowerCase(buffer),string) == 0) // If the strings match, increment the counter
-				counter++;
-			fseek(source,-(strlen(string)-1),SEEK_CUR); // Reposition stream position to next character after the matched character
-
-		}
-	}
-
-
-	fseek(source,0,SEEK_SET); // Return stream position indicator to beginning of the file
-	return counter;
-}	
-*/
+#include <getopt.h>
+#include "include/func.h" // contains custom auxiliary functions 
 
 
 int main(int argc, char *argv[]) {
 
+	// Check to see if at least three arguments have been provided
 	if(argc < 3) {
 		printf("Error: Not enough arguments\n");
 		printf("Usage: ./counter <source file> <substring 1> <substring 2> ...\n");
 		return 1;
 	}
 
+        const char * const short_options = "s";
+        const struct option long_options[] = {
+                {"systemcalls",0,NULL,'s'},
+                {NULL, 0, NULL, 0}
+        };
+
+        int next_option;
+        
+        next_option = getopt_long(argc,argv,short_options, long_options,NULL);
+
+
 	FILE *source = fopen(argv[1],"r");
+	// Check for file opening error
 	if(source == NULL) {
 		perror("Error with given file");
 		return 1;
@@ -60,17 +34,20 @@ int main(int argc, char *argv[]) {
 
 	int stringArgs, i, count;
 	stringArgs = argc-2;
+	
+	// Create an array which holds the user-provided substrings
 	char *substrings[stringArgs];
-
 	for(i = 2; i < argc; i++) 
 		substrings[i-2] = lowerCase(argv[i]);
 	
 
+	// Perform frequency count on each substring. Print the count and then process next string
 	for(i = 0; i < stringArgs; i++) {
 		count = freqCount(source,substrings[i]);
 		printf("%u\n",count);
 	}	
 
+	// Close the source file
 	fclose(source);
 	return 0;
 }
--- FrequencyCounter/func.c
+++ FrequencyCounter/func.c
@@ -1,7 +1,10 @@
 #include <string.h>
+#include <stdlib.h>
+#include <errno.h>
 #include "include/func.h"
 
 
+// This function converts all the characters in a string to lowercase
 char *lowerCase(char *arg) {
 
         int i;
@@ -15,27 +18,51 @@ char *lowerCase(char *arg) {
         return arg;
 }
 
+// Error function
+void error() {
+	perror("Error");
+	exit(1);
+}
+
+
 // This function counts the frequency of a given string in the given file.
 int freqCount(FILE *source, char *string) {
 
-        int counter;
+        int counter, seekError;
+	char *getsError;
         counter = 0;
-        char buffer[strlen(string) + 1]; // space to hold candidates from the file
+
+	// space to hold candidates from the file
+        char buffer[strlen(string) + 1]; 
 
         char c;
-        while( (c = fgetc(source)) != EOF) { // Get character from file
-                if(c == string[0]) { // If starting characters match, begin the processing
-                        fseek(source,-1,SEEK_CUR);
-                        fgets(buffer,strlen(string)+1,source); // get the candidate string from file
-                        if(strcmp(lowerCase(buffer),string) == 0) // If the strings match, increment the counter
+	// Get characters from file
+        while( (c = fgetc(source)) != EOF) { 
+		
+		// If starting characters match, begin the processing
+                if(c == string[0]) { 
+                        seekError = fseek(source,-1,SEEK_CUR);
+			if(seekError != 0)
+				error();
+	
+			// get the candidate string from file
+                        getsError = fgets(buffer,strlen(string)+1,source);
+			if(getsError == NULL)
+				error();
+
+			// If the strings match, increment the counter
+                        if(strcmp(lowerCase(buffer),string) == 0) 
                                 counter++;
-                        fseek(source,-(strlen(string)-1),SEEK_CUR); // Reposition stream position to next character after the matched character
 
-                }
-        }
+			// Reposition stream position to next character after the matched character
+
+                        fseek(source,-(strlen(string)-1),SEEK_CUR); 
+		}
+	}
 
 
-        fseek(source,0,SEEK_SET); // Return stream position indicator to beginning of the file
+	// Return stream position indicator to beginning of the file
+        fseek(source,0,SEEK_SET);
         return counter;
 }
 
