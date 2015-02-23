cf02b9ef5a4f29e7e376c6fe6e53080cd8f57990
Nikhil-Shenoy <nikhil_shenoy10@yahoo.com>
2015-02-23 10:19:01 -0500

    Finished Steps 1 and 2. Server side of program essentially done

--- Proxy/proxy-handout/proxy.c
+++ Proxy/proxy-handout/proxy.c
@@ -23,13 +23,85 @@ void format_log_entry(char *logstring, struct sockaddr_in *sockaddr, char *uri, 
 int main(int argc, char **argv)
 {
 
-    /* Check arguments */
-    if (argc != 2) {
-	fprintf(stderr, "Usage: %s <port number>\n", argv[0]);
-	exit(0);
-    }
+	int listenfd, connfd, port, clientlen;
+        struct sockaddr_in clientaddr;
 
-    exit(0);
+
+	/* Check arguments */
+	if (argc != 2) {
+		fprintf(stderr, "Usage: %s <port number>\n", argv[0]);
+		exit(0);
+	}
+
+	
+	port = atoi(argv[1]);
+	printf("Proxy started on port %u. Waiting for client connection...\n",port);
+        listenfd = Open_listenfd(port); // Creates a socket and binds it to the given port.
+
+        clientlen = sizeof(clientaddr);
+	FILE *logfile = fopen("clientLog.txt","w");
+
+	char prompt[50];
+	*prompt = "prompt";	
+	while(strcmp(prompt,"end") != 0) {
+		printf("Server is listening for client connection requests...\n");
+
+	        // Listens for requests on the listenfd, fills in client socket address in addr, returns 
+	        // connected descriptor for communication with client
+	        connfd = Accept(listenfd,(SA *)&clientaddr, &clientlen);
+        	char buffer[501];
+	        ssize_t bytesRead;
+	
+	        // Let's read a request into the buffer
+	        bytesRead = Rio_readn(connfd,buffer,500);
+	
+	
+		// Get the client IP using the clientaddr struct
+		char *clientIP;
+		clientIP = inet_ntoa(clientaddr.sin_addr);
+	
+		printf("The client's IP is: %s\n",clientIP);
+	
+		// Retrieve tokens of the request from the buffer
+		// Used an array of 10 since the URL should appear within that many tokens
+
+		char *requestTokens[10];
+		char *token;
+		token = strtok(buffer," ");
+		int i;
+		i = 0;
+		while((i != 10) && (token != NULL)) {
+			requestTokens[i] = token;
+			printf("requestTokens[%u] = %s\n",i,requestTokens[i]);
+			i++;
+			token = strtok(NULL," ");
+		}	
+	
+		// Find the URL
+		i = 0;
+		while(strcmp("GET",requestTokens[i]) != 0)
+			i++;
+	
+		// NEED TO GET THE PORT, IF AVAILABLE
+	
+		char *URL;
+		URL = requestTokens[i+1];
+
+		int logMessageLength;
+		logMessageLength = 30 + strlen(clientIP) + strlen(URL);
+		char logMessage[logMessageLength];
+		format_log_entry(logMessage,&clientaddr,URL,0);
+		printf("The logMessage is: %s\n",logMessage);
+		fprintf(logfile,"%s\n",logMessage);
+
+
+		printf("Enter the word \"end\" to end the request cycle: ");
+		scanf("%s",prompt);
+	}
+	
+	fclose(logfile);
+
+	    exit(0);
 }
 
 /*
