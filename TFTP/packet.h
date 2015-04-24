bccba23eaa0561279744f0fc39f7f322b8cc26db
Nikhil-Shenoy <nikhil_shenoy10@yahoo.com>
2015-04-23 21:10:29 -0400

    forgot the packet functions

--- TFTP/packet.h
+++ TFTP/packet.h
@@ -25,6 +25,21 @@ struct state {
 	int BytesRead;
 };
 
+struct conn {
+	uint16_t blockNum;
+	int clifd;
+	int lastPacket; // 1 for yes, 0 for no. dying
+	int open; // 1 for yes, 0 for no. available
+	int offset;
+	unsigned char filename[MAXLINE];
+	unsigned char oldData[512];
+	struct sockaddr_in *clientInfo;
+	struct sockaddr_in *serverInfo;
+	
+};
+
+	
 typedef struct state State;
 typedef struct error Error;
 typedef struct request Request;
+typedef struct conn Conn;
